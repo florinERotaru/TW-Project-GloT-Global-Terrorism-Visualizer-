f6d5eb5757f7938e076afbc33216ec06ce772b3c
Stefan Danila <100301698+stefanDanila0@users.noreply.github.com>
2023-06-18 20:42:11 +0300

    added export buttons for stat and map pages

---
+++ src/static/route.js
@@ -0,0 +1,33 @@
+// Get references to all the divs with the specified class
+const targetDivs = document.querySelectorAll('.section');
+
+// Function to handle scroll event with debounce
+function debounce(func, delay) {
+    let timer;
+    return function () {
+        clearTimeout(timer);
+        timer = setTimeout(func, delay);
+    };
+}
+
+// Function to handle URL change logic
+function handleURLChange() {
+    const viewportTop = window.scrollY;
+    const viewportBottom = viewportTop + window.innerHeight;
+
+    // Loop through the target divs and check if they are in view
+    targetDivs.forEach((div) => {
+        const divTop = div.offsetTop;
+        const divBottom = divTop + div.offsetHeight;
+
+        // Check if the div is at least 50% in the viewport
+        if (divTop <= viewportBottom - (div.offsetHeight * 0.5) && divBottom >= viewportTop + (div.offsetHeight * 0.5)) {
+            const id = div.getAttribute('id');
+            // Modify the URL using the History API
+            history.pushState({}, '', `/#${id}`); // Use the div's id in the URL
+        }
+    });
+}
+
+// Attach debounced scroll event listener to the window object
+window.addEventListener('scroll', debounce(handleURLChange, 200));
--- src/static/scrollTopButton.js
+++ src/static/scrollTopButton.js
@@ -1,10 +1,10 @@
 // Get the button:
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+  // When the user scrolls down 20px from the top of the document, show the button
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
